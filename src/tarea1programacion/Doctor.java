3931275092280ab714c68aadb70fd768ecb93216
Jonatan <Jonatan@DESKTOP-PICSI76>
2026-02-27 00:05:01 -0600

    tarea1

---
+++ src/tarea1programacion/Doctor.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea1programacion;
+
+/**
+ *
+ * @author Jonatan
+ */
+public class Doctor {
+    Integer codigo;
+    String nombredoctor;
+    String telefonodoctor;
+    String especialidad;
+
+    public Doctor(Integer codigo, String nombredoctor, String especialidad) {
+        this.codigo = codigo;
+        this.nombredoctor = nombredoctor;
+        this.especialidad = especialidad;
+    }
+
+    public Doctor() {
+    }
+    
+    
+ 
+}
---
+++ src/tarea1programacion/Medicamento.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tarea1programacion;
+
+/**
+ *
+ * @author Jonatan
+ */
+public class Medicamento {
+    Integer codigo;
+    String nombremedicamento;
+    String usomedicamento;
+    Integer stock;
+
+    public Medicamento(Integer codigo, String nombremedicamento, String usomedicamento, Integer stock) {
+        this.codigo = codigo;
+        this.nombremedicamento = nombremedicamento;
+        this.usomedicamento = usomedicamento;
+        this.stock = stock;
+    }
+
+    public Medicamento() {
+    }
+
+ 
+}
